fd06f923983b965944afc61d3c8dd5a5460d6ec4
remyjerome <remy.jerome@live.com>
2015-05-02 15:47:33 +0200

    ajax

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -1,52 +1,8 @@
-
-	/*function submitForm()
-	{
-		var xhr;
-		try
-		{
-			xhr = new ActiveXOject('Msxml2.XMLHTTP');
-		}
-		catch (e)
-		{
-			try
-			{
-				xhr = new ActiveXOject('Microsoft.XMLHTTP');
-			}
-			catch (e2)
-			{
-				try
-				{
-					xhr = new XMLHttpRequest();
-				}
-				catch (e3)
-				{
-					xhr = false;
-				}
-			}
-		}
-
-		xhr.onreadystatechange = function()
-		{
-			if(xhr.readyState == 4)
-			{
-				if(xhr.status == 200)
-					document.form_connexion.dyn="Received:" + xhr.responseText;
-				else
-					document.form_connexion.dyn="Error Code" + xhr.status;
-			}
-		};
-
-		xhr.open('POST', 'htbin/login.py', true);
-		xhr.send(null);
-	};*/
-
 (function(){
 	var form = document.getElementById('form_connection');
 
 	form.addEventListener("submit", function(e) {
 	e.preventDefault();
-    
-
     var xhr;
 		try
 		{
